4dafcf133a5ba339587800a38001e459ec1737f5
Lau02x <castrorodriguez2@gmail.com>
2025-06-28 05:27:48 -0300

    Ejercicio Calculadora, calculadora terminada

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,10 +1,87 @@
   using EspacioCalculadora;
+int num = 0;
+int activo = 1;
+double resultado = 0;
+int eleccion = 0;
+string buff;
+bool numerosCorrectos = false;
 Calculadora miCalculadora = new Calculadora();
-miCalculadora.Sumar(23);
-miCalculadora.Sumar(10);
-miCalculadora.Sumar(50);
-miCalculadora.Sumar(2);
-miCalculadora.Restar(35);
-miCalculadora.Dividir(2);
+do
+{
+    Console.WriteLine("Dar un numero para hacer una operacion:");
+    buff = Console.ReadLine();
+    if (!int.TryParse(buff, out num))
+    {
+        System.Console.WriteLine("De un numero, no una cadena");
+     }
+} while (!int.TryParse(buff, out num));
+miCalculadora.Sumar(num);
 
-Console.WriteLine($"El resultado es :  {miCalculadora.Resultado()}");
+
+while (activo == 1){
+
+    Console.WriteLine("--Elegir operacion --");
+    Console.WriteLine("1:Suma\n2:Resta\n3:Multiplicacion\n4:Division\n5:Limpiar");
+    buff = Console.ReadLine();
+    eleccion = int.Parse(buff);
+    if (eleccion == 4)
+    {
+        do
+        {
+            Console.WriteLine("Dar otro numero distinto de 0 para aplicarle la operacion:");
+            buff = Console.ReadLine();
+            num = int.Parse(buff);
+        } while (num == 0);
+    }
+    else if (eleccion == 5) { Console.WriteLine("Se limpiara el resultado"); }
+    else
+    {
+        Console.WriteLine("Dar otro numero para aplicarle la operacion:");
+        buff = Console.ReadLine();
+        num = int.Parse(buff);
+    }
+    switch (eleccion)
+    {
+        case 1:
+            miCalculadora.Sumar(num);
+            resultado = miCalculadora.Resultado();
+            Console.WriteLine($"Se sumo el numero {num}, el resultado: {resultado} ");
+            break;
+
+        case 2:
+            miCalculadora.Restar(num);
+            resultado = miCalculadora.Resultado();
+            Console.WriteLine($"Se resto el numero {num}, el resultado: {resultado} ");
+            break;
+
+        case 3:
+            miCalculadora.Multiplicar(num);
+            resultado = miCalculadora.Resultado();
+            Console.WriteLine($"Se multiplico el numero {num}, el resultado: {resultado} ");
+            break;
+
+        case 4:
+            miCalculadora.Dividir(num);
+            resultado = miCalculadora.Resultado();
+            Console.WriteLine($"Se dividio el numero {num}, el resultado: {resultado} ");
+            break;
+
+        case 5:
+            miCalculadora.Limpiar();
+            resultado = miCalculadora.Resultado();
+            Console.WriteLine($"Se reseteo la calculadora, el resultado: {resultado} ");
+            break;
+        default: break;
+    }
+    do
+    {
+        Console.WriteLine("Seguir elegiendo numeros? 1:SI, 0: Salir");
+        buff = Console.ReadLine();
+        eleccion = int.Parse(buff);
+        activo = eleccion;
+        if (eleccion == 1 || eleccion == 0)
+        {
+            numerosCorrectos = true;
+         }
+     } while (!int.TryParse(buff,out  activo) && numerosCorrectos==false);
+}
