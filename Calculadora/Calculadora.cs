810959b6799a0df4275454be58a4a8afcc1982a9
Lau02x <castrorodriguez2@gmail.com>
2025-06-28 04:47:59 -0300

    Ejercicio Calculadora, todos los metodos creados

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -3,21 +3,34 @@
     public class Calculadora
     {
         private double dato;
-        public double resultado =0;
+        public double resultado = 0;
         public void Sumar(double termino)
         {
             dato += termino;
+        }
+        public void Restar(double termino)
+        {
+            dato -= termino;
+        }
+        public void Multiplicar(double termino)
+        {
+            dato *= termino;
         }
         public void Dividir(double termino)
         {
             if (termino != 0)
             {
-                dato = dato / termino;
-             }
+                dato /= termino;
+            }
         }
-        public double GetResultado()
+        public void Limpiar()
+        {
+            dato = 0;
+        }
+        public double Resultado()
         {
             return dato;
-         }
+        }
+        
      }
 }
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,9 +1,10 @@
-// See https://aka.ms/new-console-template for more information
-using EspacioCalculadora;
+  using EspacioCalculadora;
 Calculadora miCalculadora = new Calculadora();
 miCalculadora.Sumar(23);
 miCalculadora.Sumar(10);
 miCalculadora.Sumar(50);
 miCalculadora.Sumar(2);
+miCalculadora.Restar(35);
+miCalculadora.Dividir(2);
 
-Console.WriteLine($"El resultado es :  {miCalculadora.GetResultado()}");
+Console.WriteLine($"El resultado es :  {miCalculadora.Resultado()}");
