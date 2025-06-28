5f06ff6c2a16def2fa419a602e485f53142d1049
Lau02x <castrorodriguez2@gmail.com>
2025-06-28 03:19:38 -0300

    Commit de prueba

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿namespace EspacioCalculadora
+{
+    public class Calculadora
+    {
+        private double dato;
+        public double resultado =0;
+        public void Sumar(double termino)
+        {
+            dato += termino;
+        }
+        public void Dividir(double termino)
+        {
+            if (termino != 0)
+            {
+                dato = dato / termino;
+             }
+        }
+        public double GetResultado()
+        {
+            return dato;
+         }
+     }
+}
